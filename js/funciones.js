f9228de8341f428621688e9a7ed4a0713264b3a1
proyecto <nataliepenalva123@gmail.com>
2023-06-19 15:17:09 -0300

    tercera pre-entrega

--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,81 @@
-const obtenerServicio = () => {
+function encontrarPropiedad() {
+    let div = document.getElementById("bienvenida");
+    div.className = "mt-2 text-center";
+    let h1 = document.createElement("h1");
+    h1.className = "titulo-propiedad"
+    h1.innerText = "ENCUENTRE SU PROPIEDAD";
+    div.appendChild(h1);
+};
+//Función para validar el formulario
+function validarFormulario(event) {
+    event.preventDefault();
+    const operacion = document.getElementById("operacion").value;
+    const tipoPropiedad = document.getElementById("tipoPropiedad").value;
+    const ubicacion = document.getElementById("ubicacion").value;
+    const valor = document.getElementById("valor").value;
+    let mensajeErrorOperacion = document.getElementById("mensajeErrorOperacion");
+    let mensajeErrorTipoPropiedad = document.getElementById("mensajeErrorTipoPropiedad");
+    let mensajeErrorUbicacion = document.getElementById("mensajeErrorUbicacion");
+    let mensajeErrorValor = document.getElementById("mensajeErrorValor");
+    mensajeErrorOperacion.innerHTML = "";
+    mensajeErrorTipoPropiedad.innerHTML = "";
+    mensajeErrorUbicacion.innerHTML = "";
+    mensajeErrorValor.innerHTML = "";
+    //Valido cada campo de forma individual
+    if (operacion === "operacion") {
+        mensajeErrorOperacion.innerHTML = "Por favor, selecciona una operación";
+        return false; //Detiene el programa en esta parte
+    } else {
+        mensajeErrorOperacion.innerHTML = "";
+    }
+    if (tipoPropiedad === "tipoPropiedad") {
+        mensajeErrorTipoPropiedad.innerHTML = "Por favor, selecciona una propiedad";
+        return false;
+    } else {
+        mensajeErrorTipoPropiedad.innerHTML = "";
+    }
+    if (ubicacion === "ubicacion") {
+        mensajeErrorUbicacion.innerHTML = "Por favor, selecciona una ubicación";
+        return false;
+    } else {
+        mensajeErrorUbicacion.innerHTML = "";
+    }
+    if (valor === "valor") {
+        mensajeErrorValor.innerHTML = "Por favor, selecciona un valor";
+        return false;
+    } else {
+        mensajeErrorValor.innerHTML = "";
+    }
+    document.getElementById("formulario").submit(); // Envío el formulario
+}
+/* const obtenerServicio = () => {
     let obtServicio = prompt("Busca alquilar o comprar una propiedad? (OPCIÓN: ALQUILAR/COMPRAR)").toLowerCase();
     return obtServicio;
-}
+};
 const deseaAlquilar = () =>{
     let alquilar = prompt("Desea alquilar alguna casa/ departamento? \n OPCIONES \n 1- CASA \n 2- DEPARTAMENTO \n 3- NO ESTOY INTERESADO").toLowerCase();
     return alquilar;
-}
+};
 
 const deseaComprar = () =>{
     let comprar = prompt("Desea comprar alguna casa/ departamento? \n OPCIONES \n 1- CASA \n 2- DEPARTAMENTO \n 3- NO ESTOY INTERESADO").toLowerCase();
     return comprar;
+};
+const mostrarCasasDisponibles = (propiedadesCasa) =>{
+    if (propiedadesCasa.length > 0) {
+        let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesCasa.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
+        alert(mensaje);
+        console.log("estoy en propiedad casa lenght");
+    } else {
+        alert("No hay casas disponibles en este momento.");
+    }
 }
+const mostrarDepartamentosDisponibles = (propiedadesDepartamento) =>{
+    if (propiedadesDepartamento.length > 0) {
+        let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesDepartamento.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
+        alert(mensaje);
+        console.log("estoy en propiedad casa lenght");
+    } else {
+        alert("No hay departamentos disponibles en este momento.");
+    }
+} */
--- js/main.js
+++ js/main.js
@@ -1,109 +1,107 @@
-//Procesamiento de datos
-alert("Bienvenido/a a Inmobiliaria Campana ");
-let servicio = obtenerServicio()
-while ((servicio != "alquilar") && (servicio != "comprar")) {
-    servicio = obtenerServicio()
-    console.log("estoy acá!");
-}
 //Creo una Clase alquiler y una clase comprar
-class Alquiler {
-    constructor(ubicacionPropiedad, ambientesPropiedad, tipoPropiedad, precioPropiedad, metros2) {
+class Propiedad {
+    constructor(ubicacionPropiedad, ambientesPropiedad, tipoPropiedad, precioPropiedad, metros2, estadoPropiedad,imagenPropiedad) {
         this.ubicacion = ubicacionPropiedad.toLowerCase();
         this.ambientes = ambientesPropiedad;
         this.propiedad = tipoPropiedad.toLowerCase();
         this.precio = precioPropiedad;
         this.superficie = metros2;
-        this.alquilado = false;
+        this.estado = estadoPropiedad.toLowerCase();
+        this.imagen = imagenPropiedad;
     }
 }
-const alquiler1 = new Alquiler("Zona costanera", 4, "Casa", 1400, 105.8);
-const alquiler2 = new Alquiler("Zona céntrica", 3, "Casa", 550, 197.6);
-const alquiler3 = new Alquiler("Zona céntrica", 2, "Departamento", 450, 46);
-const alquiler4 = new Alquiler("Barrio Urquiza", 3, "Departamento", 850, 92);
+const propiedad1 = new Propiedad("Zona costanera", 4, "Casa", 1400, 105.8, "comprar", "./imagenes/casa2_a.jpg");
+const propiedad2 = new Propiedad("Zona céntrica", 3, "Casa", 700, 197.6, "alquilar", "./imagenes/casa5_a.jpg");
+const propiedad3 = new Propiedad("Zona céntrica", 2, "Departamento", 450, 46, "alquilar", "./imagenes/dep1_g.jpg");
+const propiedad4 = new Propiedad("Barrio Urquiza", 3, "Departamento", 850, 92, "comprar", "./imagenes/dep2_a.jpg");
+const propiedad5 = new Propiedad("Zona céntrica", 5, "Casa", 55000, 156, "comprar","./imagenes/casa1_a.jpg");
+const propiedad6 = new Propiedad("Zona costanera", 4, "Casa", 240000, 152, "comprar", "./imagenes/casa3_a.jpg");
 
-const propiedadesEnAlquiler = [alquiler1, alquiler2, alquiler3, alquiler4];
-if (servicio === "alquilar") {
-   let ofertaAlquiler = deseaAlquilar();
-    while ((ofertaAlquiler != "casa") && (ofertaAlquiler != "departamento") && ofertaAlquiler != "no estoy interesado") {
+const propiedades = [propiedad1, propiedad2, propiedad3, propiedad4, propiedad5, propiedad6];
+//Procesamiento de datos
+encontrarPropiedad()
+//Hago referencia al formulario y al botón
+const formulario = document.querySelector("form");
+const botonFormulario = document.getElementById("botonForm");
+//Agrego un addEventListener al botón
+botonFormulario.addEventListener("click", validarFormulario); 
+// Creo mis tarjetas de productos
+const propiedadesDestacadas = document.querySelector(".propiedades");
+for(const propiedad of propiedades){
+    const card = document.createElement("div");
+    card.className = "card";
+    card.style.width = "18rem";
+    const imagen = document.createElement("img");
+    imagen.src = propiedad.imagen;
+    imagen.className = "card-img-top";
+    imagen.alt = "imagenes propiedades"
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    const title = document.createElement("h5");
+    title.className = "card-title";
+    title.textContent = propiedad.ubicacion;
+    const text = document.createElement("p");
+    text.textContent = `Casa en venta, ${propiedad.ubicacion}, ${propiedad.superficie} m2, ${propiedad.ambientes} ambientes`;
+    const link = document.createElement ("a");
+    link.className = "btn btn-primary";
+    link.href = "#";
+    link.textContent = "Ver información";
+    cardBody.appendChild(title);
+    cardBody.appendChild(text);
+    cardBody.appendChild(link);
+    card.appendChild(imagen);
+    card.appendChild(cardBody);
+    propiedadesDestacadas.appendChild(card);
+}
+/* let servicio = obtenerServicio()
+while ((servicio != "alquilar") && (servicio != "comprar")) {
+    servicio = obtenerServicio()
+    console.log("estoy acá!");
+} */
+/* if (servicio === "alquilar") {
+    let ofertaAlquiler = deseaAlquilar();
+    while ((ofertaAlquiler != "casa") && (ofertaAlquiler != "departamento") && (ofertaAlquiler != "no estoy interesado") && (ofertaAlquiler != "1") && (ofertaAlquiler != "2") && (ofertaAlquiler != "3")) {
         ofertaAlquiler = deseaAlquilar();
         console.log("hola, estoy dentro del while");
     }
-    if (ofertaAlquiler === "casa") {
-        const propiedadesCasa = propiedadesEnAlquiler.filter(prop => prop.propiedad === "casa");
-        if (propiedadesCasa.length > 0) {
-            let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesCasa.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
-            alert(mensaje);
-            console.log("estoy en propiedad casa lenght");
-        } else {
-            alert("No hay casas disponibles en este momento.");
-        }
+    if ((ofertaAlquiler === "casa") || (ofertaAlquiler === "1")) {
+        const propiedadesCasa = propiedades.filter(prop => (prop.propiedad === "casa" && prop.estado === "alquilar"));
+        mostrarCasasDisponibles(propiedadesCasa);
     }
-    if (ofertaAlquiler === "departamento") {
-        const propiedadesDepartamento = propiedadesEnAlquiler.filter(prop => prop.propiedad === "departamento");
-        if (propiedadesDepartamento.length > 0) {
-            let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesDepartamento.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
-            alert(mensaje);
-            console.log("estoy en propiedad departamento lenght");
-        }else {
-            alert("No hay departamentos disponibles en este momento");
-        }
+    if (ofertaAlquiler === "departamento" || (ofertaAlquiler === "2")) {
+        const propiedadesDepartamento = propiedades.filter(prop => (prop.propiedad === "departamento" && prop.estado === "alquilar"));
+        mostrarDepartamentosDisponibles(propiedadesDepartamento);
     }
-    if (ofertaAlquiler === "no estoy interesado") {
-        for (const propiedad of propiedadesEnAlquiler) {
+    if (ofertaAlquiler === "no estoy interesado" || (ofertaAlquiler === "3")) {
+        for (const propiedad of propiedades) {
             if (propiedad === "no estoy interesado") {
                 break;
             }
         }
     }
 }
-class Comprar {
-    constructor(ubicacionPropiedad, ambientesPropiedad, tipoPropiedad, precioPropiedad, metros2) {
-        this.ubicacion = ubicacionPropiedad.toLowerCase();
-        this.ambientes = ambientesPropiedad;
-        this.propiedad = tipoPropiedad.toLowerCase();
-        this.precio = precioPropiedad;
-        this.superficie = metros2;
-        this.comprado = false;
-    }
-}
-const comprar2 = new Comprar("Zona céntrica", 5, "Casa", 55000, 156);
-const comprar1 = new Comprar("Zona costanera", 4, "Casa", 240000, 152);
-const comprar3 = new Comprar("Zona céntrica", 2, "Departamento", 450, 46);
-const comprar4 = new Comprar("Barrio Urquiza", 3, "Departamento", 850, 92);
 
-const propiedadesEnVenta = [comprar1, comprar2, comprar3, comprar4];
 if (servicio === "comprar") {
     let ofertaComprar = deseaComprar();
-     while ((ofertaComprar != "casa") && (ofertaComprar != "departamento") && ofertaComprar != "no estoy interesado") {
-        ofertaComprar= deseaComprar();
-         console.log("hola, estoy dentro del while");
-     }
-     if (ofertaComprar === "casa") {
-         const propiedadesCasa = propiedadesEnVenta.filter(prop => prop.propiedad === "casa");
-         if (propiedadesCasa.length > 0) {
-             let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesCasa.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
-             alert(mensaje);
-             console.log("estoy en propiedad casa lenght");
-         } else {
-             alert("No hay casas disponibles en este momento.");
-         }
-     }
-     if (ofertaComprar === "departamento") {
-         const propiedadesDepartamento = propiedadesEnVenta.filter(prop => prop.propiedad === "departamento");
-         if (propiedadesDepartamento.length > 0) {
-             let mensaje = "Las opciones disponibles en este momento son: \n\n" + propiedadesDepartamento.map(prop => `Propiedad: ${prop.propiedad} \n Ubicada en: ${prop.ubicacion} \n Precio: ${prop.precio} USD \n\n`).join("");
-             alert(mensaje);
-             console.log("estoy en propiedad departamento lenght");
-         }else {
-             alert("No hay departamentos disponibles en este momento");
-         }
-     }
-     if (ofertaComprar === "no estoy interesado") {
-         for (const propiedad of propiedadesEnVenta) {
-             if (propiedad === "no estoy interesado") {
-                 break;
-             }
-         }
-     }
+    while ((ofertaComprar != "casa") && (ofertaComprar != "departamento") && (ofertaComprar != "no estoy interesado") && (ofertaComprar != "1") && (ofertaComprar != "2") && (ofertaComprar != "3")) {
+        ofertaComprar = deseaComprar();
+        console.log("hola, estoy dentro del while");
+    }
+    if ((ofertaComprar === "casa") || (ofertaComprar === "1")) {
+        const propiedadesCasa = propiedades.filter(prop => (prop.propiedad === "casa" && prop.estado === "comprar"));
+        mostrarCasasDisponibles(propiedadesCasa);
+    }
+    if (ofertaComprar === "departamento" || (ofertaComprar === "2")) {
+        const propiedadesDepartamento = propiedades.filter(prop => (prop.propiedad === "departamento" && prop.estado === "comprar"));
+        mostrarDepartamentosDisponibles(propiedadesDepartamento);
+    }
+    if (ofertaComprar === "no estoy interesado" || (ofertaComprar === "3")) {
+        for (const propiedad of propiedades) {
+            if (propiedad === "no estoy interesado") {
+                break;
+            }
+        }
+    }
 }
 
+ */
