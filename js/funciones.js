7307afcd69d12c3fc0ddc21ce873f417cc947fb8
proyecto <nataliepenalva123@gmail.com>
2023-08-01 11:22:10 -0300

    cambio de ruta heart.svg

--- js/funciones.js
+++ js/funciones.js
@@ -46,7 +46,7 @@ function cantidadTotalPropiedades() {
 function renderBotonFavs() {
     let botonFavs = document.getElementById("favs");
     let contenido = `<button type="button" class="btn btn-primary position-relative">
-        <img class="corazon_img" src="../imagenes/heart.svg" alt="corazón">
+        <img class="corazon_img" src="./imagenes/heart.svg" alt="corazón">
         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
             ${cantidadTotalPropiedades()}  
         </span>
