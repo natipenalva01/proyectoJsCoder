4ed8b741a3fb34ef57eab222cd68aa9bbaa5c04e
proyecto <nataliepenalva123@gmail.com>
2023-08-04 19:22:20 -0300

    modificación en página favoritos

--- js/favoritos.js
+++ js/favoritos.js
@@ -10,7 +10,7 @@ function renderPropiedades() {
             <td><img src="${propiedad.imagen}" alt="propiedades disponibles" width = 90px height = 90px></td>
             <td><h5 class="card-title"><b>${propiedad.precio}</b></h5></td>
             <td><p class="card-text text-muted"> Propiedad en ${(propiedad.estado).toLowerCase()}, ubicada en ${propiedad.ubicacion}</p></td>
-            <td> <img src="./imagenes/trash3.svg" class="trash"alt="Eliminar propiedad" width = "24" onclick="eliminarPropiedad(${propiedad.codigo})"></td>
+            <td> <img src="../imagenes/trash3.svg" class="trash"alt="Eliminar propiedad" width = "24" onclick="eliminarPropiedad(${propiedad.codigo})"></td>
             </tr>
             `;
         });
